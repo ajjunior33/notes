39c2ad82d6ad781b5160d090901825a1a15a57fe
André Jr <ajjunior33@gmail.com>
2020-11-09 09:43:43 -0300

    Include env port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,7 @@
-const http = require('http');
-const app = require('./app/app');
-const server =http.createServer(app);
-server.listen(3333);
+const http = require("http");
+const app = require("./app/app");
+const server = http.createServer(app);
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`Server runnig port ${port}`);
+});
